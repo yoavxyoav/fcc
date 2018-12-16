6b693424a4a951b75277412c01fc096be1f7f6d8
Yoav Vollansky <yoavxxyoav@gmail.com>
2018-12-16 15:59:08 +0000

    Create higher-order-arrow-functions.js

---
+++ higher-order-arrow-functions.js
@@ -0,0 +1,14 @@
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+  const squaredIntegers = (arr.filter((n) => n > 0 && n == Math.floor(n))).map((x) => x*x);
+
+
+
+  // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
