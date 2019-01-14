9697171e6ac4788e68f0e7d43c48bc4930c8f305
Yoav Vollansky <yoavxxyoav@gmail.com>
2019-01-14 02:16:09 +0000

    Create DiffArray.js

---
+++ DiffArray.js
@@ -0,0 +1,21 @@
+function diffArray(arr1, arr2) {
+  var a1 = [].concat(arr1);
+  var a2 = [].concat(arr2);
+  
+
+  for (let item of arr1) {
+    if (arr2.includes(item)) {
+      a1.splice(a1.indexOf(item),1)
+      a2.splice(a2.indexOf(item),1)
+    }
+  }
+
+  for (let item of a2) {
+    if (a1.includes(item)) {
+      a1.splice(a1.indexOf(item),1)
+      a2.splice(a2.indexOf(item),1)      
+    }
+  }
+  
+  return a1.concat(a2);
+}
