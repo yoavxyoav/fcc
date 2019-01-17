174fc6eaf2f685ba9414ba305e23241d87848b62
Yoav Vollansky <yoavxxyoav@gmail.com>
2019-01-17 02:10:42 +0000

    added some comments for clarification

--- whatIsInName.js
+++ whatIsInName.js
@@ -1,25 +1,34 @@
 function whatIsInAName(collection, source) {
-
+  // the result array
   var arr = [];
-  console.log(Object.entries(source))
-  console.log(collection)
 
-  for (let item of collection) {
-    console.log("\n^^^ " + Object.entries(item))
-    let allIn = true
+  
+  console.log(Object.entries(source))
+  console.log(Object.entries(collection))
+  // iterating over the bigger list, dealing with each object as 'obj'
+  for (let obj of collection) {
+    let allIn = true //setting the marker to true 
+    //iterating over the smaller list as key:value pairs
     for (let otherItem of Object.entries(source)) {
-      console.log("--- " + otherItem)
-      if (!Object.entries(item).includes(otherItem)){
-        allIn = false
-        console.log("!", Object.entries(item))
-        break;
+      console.log("---", otherItem)
+      // if key:value pairs of obj do not include the other item
+      if (!Object.entries(obj).includes(otherItem)){
+        allIn = false // then the marker is set to false
+        console.log("!", obj.first)
+        break; // and the loop breaks
       }
     }
-    if (allIn = true) {
-      arr.push(item)
+    if (allIn = true) { // and only if the marker hasn't been set to false
+      arr.push(obj) // then the result array is pushed with the object
     }
   }
 
 
+  // Only change code below this line
+  
+  
+  // Only change code above this line
   return arr;
 }
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
