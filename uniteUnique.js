ba6ec60ba68400cf83cb9483eacdd31aa8188c00
Yoav Vollansky <yoavxxyoav@gmail.com>
2019-01-29 00:41:26 +0000

    Update uniteUnique.js

--- uniteUnique.js
+++ uniteUnique.js
@@ -1,21 +1,12 @@
 function uniteUnique(arr) {
-  
-  let seenBefore = []
-  
-  console.log(typeof(seenBefore) == "object")
-
-  for (let item of arr) {
-    console.log(item)
-    if (typeof(item) == "object") {
-      console.log("it's an object!!")
-      uniteUnique(item)
-    } else if (!seenBefore.includes(item)) {
-      seenBefore.push(item)
+    let seenBefore = []; //the accumulating array
+    for (let item of arguments) {
+        if (typeof (item) == "object") {
+            uniteUnique(...item);
+        }
+        else if (!seenBefore.includes(item)) {
+            seenBefore.push(item);
+        }
     }
-  }
-  
-  console.log(seenBefore)
-  return seenBefore;
+    return seenBefore;
 }
-
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
