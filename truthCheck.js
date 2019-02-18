fdeae4b31dd681fa165e59c457ee45b377325596
Yoav Vollansky <yoavxxyoav@gmail.com>
2019-02-18 23:56:33 +0000

    Update truthCheck.js
    code corrected

--- truthCheck.js
+++ truthCheck.js
@@ -1,17 +1,17 @@
+// Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
 function truthCheck(collection, pre) {
-    let allThere = true
     for (let item of collection) {
         if (!item.hasOwnProperty(pre)) {
             return false
-        } else if (item.hasOwnProperty(pre) && (item[pre] == false || item[pre] == null || item[pre] === NaN)) {
-            return false
+        } else if (item.hasOwnProperty(pre) && (item[pre] == false || item[pre] == null)) {
+          return false
+        } else if ( item.hasOwnProperty(pre) && item[pre] !== item[pre]) { // this line is important. isNaN() doesn't work, but checking if the variable is different than itself does.
+          return false
           
         }
     }
-    return allThere;
+    return true;
   }
   
-  
-  let x = truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": NaN}], "sex");
-
-  console.log(x)
+  truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
